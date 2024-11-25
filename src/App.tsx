3f6b0e888c2b0440710ab262621917e4a00d6b38
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-11-25 18:29:24 +0000

    Theme config

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "styled-components"
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle/>
+        <h1>Teste</h1>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
